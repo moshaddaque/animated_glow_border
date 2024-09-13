5d63ee09b147fc503cc0dda57ce8c9df0c0e3a42
moshaddaque <info.moshaddaque@gmail.com>
2024-09-14 01:42:37 +0600

    Updated the gradient animation and glow effect

--- lib/src/glow_border_widget.dart
+++ lib/src/glow_border_widget.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AnimatedGlowBorder extends StatefulWidget {
   final Widget child;
   final double borderWidth;
-  final BorderRadius borderRadius;
+  final double borderRadius;
   final Duration animationDuration;
   final Color startColor;
   final Color endColor;
@@ -12,7 +12,7 @@ class AnimatedGlowBorder extends StatefulWidget {
     super.key,
     required this.child,
     this.borderWidth = 5.0,
-    this.borderRadius = const BorderRadius.all(Radius.circular(12)),
+    this.borderRadius = 0,
     this.animationDuration = const Duration(seconds: 3),
     this.startColor = Colors.blue,
     this.endColor = Colors.purple,
@@ -48,6 +48,7 @@ class _AnimatedGlowBorderState extends State<AnimatedGlowBorder>
       begin: widget.endColor,
       end: widget.startColor,
     ).animate(_controller);
+    _controller.repeat();
   }
 
   @override
@@ -58,41 +59,84 @@ class _AnimatedGlowBorderState extends State<AnimatedGlowBorder>
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _controller,
-      builder: (context, child) {
-        return Container(
-          decoration: BoxDecoration(
-            borderRadius: widget.borderRadius,
-            border: Border.all(
-              width: widget.borderWidth,
-              color: Colors.transparent,
-            ),
-            gradient: LinearGradient(
-              colors: [
-                _colorAnimation1.value!,
-                _colorAnimation2.value!,
+    return ClipPath(
+      clipper: _CenterCutPath(
+          radius: widget.borderRadius, thikness: widget.borderWidth),
+      child: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, child) {
+          return Container(
+            decoration: BoxDecoration(
+              // borderRadius: widget.borderRadius,
+              border: Border.all(
+                width: widget.borderWidth,
+                color: Colors.transparent,
+              ),
+              gradient: LinearGradient(
+                colors: [
+                  _colorAnimation1.value!,
+                  _colorAnimation2.value!,
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: _colorAnimation1.value!.withOpacity(0.6),
+                  blurRadius: 20,
+                  spreadRadius: 4,
+                ),
+                BoxShadow(
+                  color: _colorAnimation2.value!.withOpacity(0.6),
+                  blurRadius: 20,
+                  spreadRadius: 4,
+                ),
               ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
             ),
-            boxShadow: [
-              BoxShadow(
-                color: _colorAnimation1.value!.withOpacity(0.6),
-                blurRadius: 20,
-                spreadRadius: 4,
-              ),
-              BoxShadow(
-                color: _colorAnimation2.value!.withOpacity(0.6),
-                blurRadius: 20,
-                spreadRadius: 4,
-              ),
-            ],
-          ),
-          child: widget.child,
-        );
-      },
-      child: widget.child,
+            child: widget.child,
+          );
+        },
+        child: widget.child,
+      ),
     );
+  }
+}
+
+class _CenterCutPath extends CustomClipper<Path> {
+  final double radius;
+  final double thikness;
+
+  _CenterCutPath({
+    this.radius = 0,
+    this.thikness = 1,
+  });
+
+  @override
+  Path getClip(Size size) {
+    final rect = Rect.fromLTRB(
+      -size.width,
+      -size.width,
+      size.width * 2,
+      size.width * 2,
+    );
+    final double width = size.width - thikness * 2;
+    final double height = size.height - thikness * 2;
+
+    final path = Path()
+      ..fillType = PathFillType.evenOdd
+      ..addRRect(
+        RRect.fromRectAndRadius(
+          Rect.fromLTWH(thikness, thikness, width, height),
+          Radius.circular(radius - thikness),
+        ),
+      )
+      ..addRect(rect);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant _CenterCutPath oldClipper) {
+    return oldClipper.radius != radius || oldClipper.thikness != thikness;
   }
 }
